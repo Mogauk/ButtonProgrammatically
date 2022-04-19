2c7321c874c1278500f5f33f126653499ef8239f
Alexey <line6@mail.ru>
2022-04-19 10:46:28 +0300

    Initial Commit

---
+++ ButtonProgrammatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ButtonProgrammatically
+//
+//  Created by Alexey on 4/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
